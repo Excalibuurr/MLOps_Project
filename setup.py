1902a16457e038f481b8fb1d8795443527b80d40
Excalibuurr <legendaryyusuf8@gmail.com>
2025-09-02 01:42:33 +0530

    first commit created project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This an end to end ml project with deployement that is production ready as well as use by the industry.',
+    author='Yusuf Ansari',
+    license='MIT',
+)
